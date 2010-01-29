a2e0fe7d4a2e611910a51ccf47d478e8a5a18301
Matt Turner <matturner@gmail.com>
2010-01-29 00:28:23 +0000

    compare_dotted_version() unit tests

--- tests/unit/common_test.c
+++ tests/unit/common_test.c
@@ -12,7 +12,7 @@
 #include "tests.h"
 
 
-void common_test (void)
+static void test_basename_dirname (void)
 {
     const char *a = "/foo/bar/baz",
                *b = "/foo",
@@ -34,4 +34,58 @@ void common_test (void)
 
     assert(!strcmp(fsfuse_basename(e), ""));
     assert(!strcmp(fsfuse_dirname(e), ""));
+}
+
+static void test_compare_dotted_version (void)
+{
+    assert(compare_dotted_version("1", "2") == -1);
+    assert(compare_dotted_version("2", "1") == 1);
+    assert(compare_dotted_version("1", "1") == 0);
+    assert(compare_dotted_version("0", "0") == 0);
+
+    assert(compare_dotted_version("0.1", "0.2") == -1);
+    assert(compare_dotted_version("0.2", "0.1") == 1);
+    assert(compare_dotted_version("0.1", "0.1") == 0);
+    assert(compare_dotted_version("0.0", "0.0") == 0);
+
+    assert(compare_dotted_version("0.5.1", "0.5.2") == -1);
+    assert(compare_dotted_version("0.5.2", "0.5.1") == 1);
+    assert(compare_dotted_version("0.5.1", "0.5.1") == 0);
+    assert(compare_dotted_version("0.5.0", "0.5.0") == 0);
+
+    assert(compare_dotted_version("0.2", "0.2.0") == 0);
+    assert(compare_dotted_version("0.2", "0.2.1") == -1);
+    assert(compare_dotted_version("0.2", "0.2.2") == -1);
+    assert(compare_dotted_version("0.2", "0.2.3") == -1);
+    assert(compare_dotted_version("0.2", "0.1.9") == 1);
+
+    assert(compare_dotted_version("0.2.0", "0.2") == 0);
+    assert(compare_dotted_version("0.2.1", "0.2") == 1);
+    assert(compare_dotted_version("0.2.2", "0.2") == 1);
+    assert(compare_dotted_version("0.2.3", "0.2") == 1);
+    assert(compare_dotted_version("0.1.9", "0.2") == -1);
+
+    assert(compare_dotted_version("0.2", "0.2.0.0.1") == -1);
+    assert(compare_dotted_version("0.2.0.0.1", "0.2") == 1);
+
+    assert(compare_dotted_version("1", "666") == -1);
+    assert(compare_dotted_version("11111.11111.22", "11111.11111.22") == 0);
+
+    assert(compare_dotted_version("0.10", "0.1") == 1);
+    assert(compare_dotted_version("0.10", "0.1.0") == 1);
+    assert(compare_dotted_version("0.10", "0.10") == 0);
+    assert(compare_dotted_version("0.10", "0.10.0") == 0);
+    assert(compare_dotted_version("0.10", "0.11") == -1);
+
+    assert(compare_dotted_version("0.1", "0.10") == -1);
+    assert(compare_dotted_version("0.1.0", "0.10") == -1);
+    assert(compare_dotted_version("0.10", "0.10") == 0);
+    assert(compare_dotted_version("0.10.0", "0.10") == 0);
+    assert(compare_dotted_version("0.11", "0.10") == 1);
+}
+
+void common_test (void)
+{
+    test_basename_dirname();
+    test_compare_dotted_version();
 }
